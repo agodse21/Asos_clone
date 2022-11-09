e79078b542fe5c395d0e1167a78b218b2e4feff4
Rautmahi <100849820+Rautmahi@users.noreply.github.com>
2022-11-10 00:13:28 +0530

    landing page data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import LandingPage from './Components/LandingPage';
 
 function App() {
   return (
     <div className="App">
      <h1>hello</h1>
+     <LandingPage/>
     </div>
   );
 }
---
+++ frontend/src/Components/LandingPage.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div style={{ width: "95%","margin":"auto"}}>
+        <img src="https://content.asos-media.com/-/media/homepages/unisex/homepages/2022/11-nov/singles-day---click-frenzy/artworked-promos/high-awareness-markets/30/row/2022_singles-day_high-awareness_30-homepage-promo-hero-desktop_ww_row_1258x600.jpg" alt=""/>
+      </div>
+      <br />
+      <br />
+      {/* 2nd div */}
+      <div style={{ width: "95%", margin: "auto" ,"display":"flex","justifyContent":"space-between"}}>
+        <div style={{ width: "24%"}}>
+          <img src="https://content.asos-media.com/-/media/homepages/unisex/homepages/2022/11-nov/singles-day---click-frenzy/prime/moments-features-with-borders/ww/ww---self-care.jpg" alt=""/>
+          <h3 style={{"fontWeight":"bold","fontSize":"18px"}}>SELF CARE MOMENT </h3>
+          <p>It's treats Yourself Time</p>
+        </div>
+
+        <div style={{ width: "24%"}}>
+          <img src="https://content.asos-media.com/-/media/homepages/unisex/homepages/2022/11-nov/singles-day---click-frenzy/prime/moments-features-with-borders/ww/ww---label-love.jpg" alt=""/>
+          <h3 style={{"fontWeight":"bold","fontSize":"18px"}}> LABEL LOVE </h3>
+          <p>It's treats Yourself Time</p>
+        </div>
+
+        <div style={{ width: "24%"}}>
+          <img src="https://content.asos-media.com/-/media/homepages/unisex/homepages/2022/11-nov/singles-day---click-frenzy/prime/moments-features-with-borders/ww/ww---in-house-brands.jpg" alt=""/>
+          <h3 style={{"fontWeight":"bold","fontSize":"18px"}}> IN-HOUSE BRNDS</h3>
+          <p>It's treats Yourself Time</p>
+        </div>
+
+        <div style={{ width: "24%"}}>
+          <img src="https://content.asos-media.com/-/media/homepages/unisex/homepages/2022/11-nov/singles-day---click-frenzy/prime/moments-features-with-borders/ww/ww---go-for-glam.jpg" alt=""/>
+          <h3 style={{"fontWeight":"bold","fontSize":"18px"}}> GO FOR GLAM </h3>
+          <p>It's treats Yourself Time</p>
+        </div>
+      </div>
+<br />
+<br />
+      {/* 3rd div */}
+
+      <div style={{"width":"100%","height":"115px","backgroundColor":"#f799ba"}}> 
+        <p style={{"fontWeight":"bold","fontSize":"28px","fontStyle":"italic","color":"darkblack"}}>UPTO 50% OFF COSY GEAR! </p>
+        <p style={{"fontWeight":"bold","fontSize":"28px","fontStyle":"italic"}}>GIVE WINTER THE COLD SHOULDER </p>
+        <p  style={{"fontSize":"10px"}}>Limited time only.Selected styles marked down as shown</p>
+      </div>
+       <br />
+      <br />
+
+      {/* after offer page 2 div */}
+      <div style={{ width: "95%", margin: "auto" ,"display":"flex","justifyContent":"space-evenly"}}>
+        <div style={{ width: "40%"}}>
+          <img src="https://content.asos-media.com/-/media/homepages/unisex/homepages/2022/11-nov/singles-day---click-frenzy/prime/moments-features-with-borders/ww/ww--treat-yoself-brands.jpg"alt="" />
+          <h3 style={{"fontWeight":"bold","fontSize":"30px"}}>TREAT YO,SELF BRANDS </h3>
+          <p>Cos deserve the best</p>
+          <br />
+          <button style={{"fontSize":"25px","border":"2px solid black","padding":"10px"}}> SHOP NOW</button>
+
+        </div>
+
+        <div style={{ width: "40%"}}>
+          <img src="https://content.asos-media.com/-/media/homepages/unisex/homepages/2022/11-nov/singles-day---click-frenzy/prime/moments-features-with-borders/ww/ww---collusion.jpg" alt=""/>
+          <h3 style={{"fontWeight":"bold","fontSize":"30px"}}> COLLUSIONS </h3>
+          <p>POV:the club in 2062</p>
+          <br />
+          <button style={{"fontSize":"25px","border":"2px solid black","padding":"10px"}}> SHOP THE BRAND</button>
+        </div>
+
+      </div>
+      
+<br />
+<br />
+{/* get gifting div */}
+<div style={{"width":"100%","height":"115px"}}>
+<img src=" https://content.asos-media.com/-/media/hubs/gifting/2022/01_jpeg/gifting/dt_live-text-banner_2880x600.jpg" alt=""/>
+</div>
+<br />
+<br />
+<br />
+<br />
+
+{/* trending brands */}
+<div style={{"display":"flex","margin":"auto","justifyContent":"space-evenly"}}> 
+<img style={{"width":"16%"}} src="https://content.asos-media.com/-/media/homepages/unisex/brands-logos/256x256/monkl-hp-logos-256x256.jpg" alt="" />
+<img style={{"width":"16%"}} src="https://content.asos-media.com/-/media/homepages/unisex/brands-logos/256x256/never-fully-dressed-hp-logos-256x256.png" alt="" />
+<img style={{"width":"16%"}} src="https://content.asos-media.com/-/media/homepages/unisex/brands-logos/256x256/1x12x---test.png" alt="" />
+<img style={{"width":"16%"}} src="https://content.asos-media.com/-/media/homepages/unisex/brands-logos/256x256/reclaimed-vintage-hp-logos-256x256.jpg" alt="" />
+<img style={{"width":"16%"}} src="https://content.asos-media.com/-/media/homepages/unisex/brands-logos/256x256/collusion-hp-logos-256x256.jpg" alt="" />
+<img style={{"width":"16%"}} src="https://content.asos-media.com/-/media/homepages/unisex/brands-logos/256x256/weekday-hp-logos-256x256.jpg" alt="" />
+</div>    
+<br />
+<br />
+<hr />
+<br />
+<br />
+{/* social media img */}
+
+<div>
+    
+</div>
+    </div>
+  );
+};
+
+export default LandingPage;
