3bc00997590ff4a623c720b6e152de46b1e24f9a
sudiptadip <sudiptabhattacharjee000@gmail.com>
2022-11-12 18:15:23 +0530

    added

--- frontend/src/Redux/rootReducer.js
+++ frontend/src/Redux/rootReducer.js
@@ -1,14 +1,12 @@
 import {combineReducers} from "redux"
 import {reducers as AppReducer} from "./App/reducer"
-// import {reducer as AuthReducer} from "../Auth/reducer";
+import { AuthReducer } from "./Auth/reducer";
 // import {reducer as CartReducer} from "../Cartreducer/reducer"
 
 const rootreducer=combineReducers({
     AppReducer,
-    // AuthReducer
+    AuthReducer
     // CartReducer,
-   
-
 })
 
 export default rootreducer;
