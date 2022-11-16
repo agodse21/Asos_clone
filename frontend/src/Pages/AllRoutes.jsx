1e40ea7776caa816adedf3420c0bb2c530c35d4b
agodse21 <agodse9@gmail.com>
2022-11-16 20:09:23 +0530

    dynamic bag count change accourding to length

--- frontend/src/Pages/AllRoutes.jsx
+++ frontend/src/Pages/AllRoutes.jsx
@@ -66,35 +66,35 @@ export const AllRoutes = () => {
         <Route path="/womensale" element={<RequireAuth><WomenSale /></RequireAuth>} />
         <Route path="/womenbrand" element={<RequireAuth><WomenBrand /></RequireAuth>} />
         <Route path="/womenoutlet" element={<RequireAuth><WomenOulet /></RequireAuth>} />
-        <Route path="/womenmarketplace" element={<WomenMarketPlace />} />
-        <Route path="/womenfacebody" element={<WomenFaceBody />} />
-        <Route path="/womennewin" element={<WomenNewIn />} />
-        <Route path="/womenclothing" element={<WomenCloathing />} />
-        <Route path="/womendresses" element={<WomenDresses />} />
-        <Route path="/womenshoe" element={<WomenShoes />} />
-        <Route path="/womensportwear" element={<WomenSportWear />} />
-        <Route path="/womenaccessories" element={<WomenAccessoris />} />
-        <Route path="/womenautumn" element={<WomenAutumn />} />
-        <Route path="/womengifting" element={<WomenGifting />} />
+        <Route path="/womenmarketplace" element={<RequireAuth><WomenMarketPlace /></RequireAuth>} />
+        <Route path="/womenfacebody" element={<RequireAuth><WomenFaceBody /></RequireAuth>} />
+        <Route path="/womennewin" element={<RequireAuth><WomenNewIn /></RequireAuth>} />
+        <Route path="/womenclothing" element={<RequireAuth><WomenCloathing /></RequireAuth>} />
+        <Route path="/womendresses" element={<RequireAuth><WomenDresses /></RequireAuth>} />
+        <Route path="/womenshoe" element={<RequireAuth><WomenShoes /></RequireAuth>} />
+        <Route path="/womensportwear" element={<RequireAuth><WomenSportWear /></RequireAuth>} />
+        <Route path="/womenaccessories" element={<RequireAuth><WomenAccessoris /></RequireAuth>} />
+        <Route path="/womenautumn" element={<RequireAuth><WomenAutumn /></RequireAuth>} />
+        <Route path="/womengifting" element={<RequireAuth><WomenGifting /></RequireAuth>} />
         <Route path="/menhome" element={<MenPage />} />
 
         <Route path="/mensale" element={<RequireAuth><MenSale /></RequireAuth>} />
-        <Route path="/mentopshop" element={<MenTopShop />} />
-        <Route path="/menbrand" element={<MenBrand />} />
-        <Route path="/menoutlet" element={<MenOulet />} />
-        <Route path="/menmarketplace" element={<MenMarketPlace />} />
-        <Route path="/menfacebody" element={<MenFaceBody />} />
-        <Route path="/mennewin" element={<MenNewIn />} />
-        <Route path="/menclothing" element={<MenCloathing />} />
-        <Route path="/mendresses" element={<MenDresses />} />
-        <Route path="/menshoe" element={<MenShoes />} />
-        <Route path="/mensportwear" element={<MenSportWear />} />
-        <Route path="/menaccessories" element={<MenAccessoris />} />
-        <Route path="/menautumn" element={<MenAutumn />} />
-        <Route path="/mengifting" element={<MenGifting />} />
+        <Route path="/mentopshop" element={<RequireAuth><MenTopShop /></RequireAuth>} />
+        <Route path="/menbrand" element={<RequireAuth><MenBrand /></RequireAuth>} />
+        <Route path="/menoutlet" element={<RequireAuth><MenOulet /></RequireAuth>} />
+        <Route path="/menmarketplace" element={<RequireAuth><MenMarketPlace /></RequireAuth>} />
+        <Route path="/menfacebody" element={<RequireAuth><MenFaceBody /></RequireAuth>} />
+        <Route path="/mennewin" element={<RequireAuth><MenNewIn /></RequireAuth>} />
+        <Route path="/menclothing" element={<RequireAuth><MenCloathing /></RequireAuth>} />
+        <Route path="/mendresses" element={<RequireAuth><MenDresses /></RequireAuth>} />
+        <Route path="/menshoe" element={<RequireAuth><MenShoes /></RequireAuth>} />
+        <Route path="/mensportwear" element={<RequireAuth><MenSportWear /></RequireAuth>} />
+        <Route path="/menaccessories" element={<RequireAuth><MenAccessoris /></RequireAuth>} />
+        <Route path="/menautumn" element={<RequireAuth><MenAutumn /></RequireAuth>} />
+        <Route path="/mengifting" element={<RequireAuth><MenGifting /></RequireAuth>} />
         <Route path="/productdetails/:id" element={<RequireAuth><SinglePageProduct /></RequireAuth>} />
-           <Route path="/cart" element={<Maincartpage/>}></Route>
-           <Route path="/searchproduct" element={<SearchPage />}></Route>
+           <Route path="/cart" element={<RequireAuth><Maincartpage/></RequireAuth>}></Route>
+           <Route path="/searchproduct" element={<RequireAuth><SearchPage /></RequireAuth>}></Route>
            
       </Routes>
 <Footer />
